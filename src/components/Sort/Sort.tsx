29e213f750d890dd7b9fbfa7f33fbcc7a72e3ceb
Sirida <nik.sviridov.1999@mail.ru>
2022-07-04 18:05:52 +0300

    add-sort-modal

--- src/components/Sort/Sort.tsx
+++ src/components/Sort/Sort.tsx
@@ -1,10 +1,29 @@
+import { useState } from "react";
 import classes from "./Sort.module.scss";
 
 const Sort = () => {
+  const [openSelect, setOpenSelect] = useState(false);
   return (
     <div>
       <b>Сортировка по: </b>
-      <span className={classes["sort"]}>популярности</span>
+      <span
+        onClick={() => setOpenSelect(!openSelect)}
+        className={classes["sort"]}
+      >
+        популярности
+      </span>
+      {openSelect && (
+        <div className={classes["sort-modal"]}>
+          <ul>
+            <li>популярности</li>
+            <li>популярности</li>
+            <li>популярности</li>
+            <li>популярности</li>
+            <li>популярности</li>
+            <li>популярности</li>
+          </ul>
+        </div>
+      )}
     </div>
   );
 };
